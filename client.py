1aba2e30dd03b945d5c6a9ba22aaa2dbea961ce2
Tuan <88710207+tuannbdz@users.noreply.github.com>
2022-12-01 18:09:23 +0700

    pdf download

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 from socket import *
-serverName = 'example.com'
+serverName = 'web.stanford.edu'
 serverPort = 80
 MAXBUF = 1024
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-requestMessage = "GET /index.html HTTP/1.1\r\n"
+requestMessage = "GET http://web.stanford.edu/class/cs224w/slides/01-intro.pdf HTTP/1.1\r\n"
 connectionType = "Connection: keep-alive\r\n"
 hostName = "Host: www." + serverName + "\r\n"
 persistentConnectionType = "Transfer-Encoding: chunked\r\n"
@@ -21,35 +21,45 @@ print("Byte send: ", iResult)
 fragments = []
 i = 10
 while True:
-    chunk = clientSocket.recv(MAXBUF)
-    fragments.append(chunk)
-    print(len(chunk))
-    if len(chunk) == 0:
-        break
-
-    temp = chunk.decode().lower()
-    if(temp.find('content-length') >= 0):
-        posHeader = temp.find('content-length') + 15
-        fileLen = int(temp[posHeader : temp.find('\r\n', posHeader)])
+    firstChunk = clientSocket.recv(MAXBUF)
+    fragments.append(firstChunk)
+    print(len(firstChunk))
+    temp = firstChunk
+    
+    # print(chunk)
+    if(temp.find(b'Content-Length') >= 0):
+        posHeader = temp.find(b'Content-Length') + 15
+        fileLen = int(temp[posHeader : temp.find(b'\r\n', posHeader)])
         print('fileLen: ', fileLen)
         offset = 0
-        if temp.find('\r\n\r\n'):
-            BodyBegin = temp.find('\r\n\r\n') + 4
-            offset = len(chunk) - BodyBegin + 1
-            if offset == fileLen:
-                print("Finish receiving data...")
-            else:
-                remain = clientSocket.recv(fileLen - offset)
-                fragments.append(remain)
+        if temp.find(b'\r\n\r\n'):
+            BodyBegin = temp.find(b'\r\n\r\n') + 4
+            offset = len(firstChunk) - BodyBegin + 1
+            if offset != fileLen:
+                remain = (fileLen - offset)
+                cntByteRecv = 0
+                while cntByteRecv < remain:
+                    chunk = clientSocket.recv(remain - cntByteRecv)
+                    cntByteRecv += len(chunk)
+                    fragments.append(chunk)
+                    print('Downloading ', cntByteRecv / remain * 100, '%')
+            print("Finish receiving data...")
             break
     elif (temp.find('transfer-encoding: chunked')):
         break
 
 receiveMessage = b''.join(fragments)
 # print("Data: ", receiveMessage)
-
-f = open("example.html", "w")
-f.write(receiveMessage.decode('latin-1'))
+receiveMessage = receiveMessage.split(b'\r\n\r\n')
+headers = receiveMessage[0]
+data = b''
+for i in range(1, len(receiveMessage)):
+    data += receiveMessage[i]
+# f = open("example.html", "w")
+# print(headers)
+# print(data)
+f = open("example.pdf", 'wb')
+f.write(data)
 # f.write(receiveMessage[receiveMessage.find('<') : ])
 f.close()
 clientSocket.close()
