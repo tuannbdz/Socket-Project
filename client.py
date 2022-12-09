a7fa7bffa8a4eca9f1deabaa7ae408b768c9d0ae
Tuan <88710207+tuannbdz@users.noreply.github.com>
2022-12-10 00:27:38 +0700

    optimize space

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
-from threading import Thread
+# from threading import Thread
 import concurrent.futures
 import threading
 import time
 
+import pdb
 import socket
 import sys
 import os
 
+# pdb.set_trace()
 MAXBUF = 8192
 serverPort = 80
 curDir = os.path.dirname(os.path.realpath(__file__))
@@ -16,78 +18,6 @@ def initClientSocket():
     clientSocket.settimeout(5)
     return clientSocket
     
-def messageReceive(clientSocket):
-    dataChunks = []
-    while True:
-        firstChunk = clientSocket.recv(MAXBUF)
-        if(firstChunk.find(b'404 Not Found') >= 0):
-            raise Exception('File not found in server...')
-        # print(firstChunk)
-        rollingBuffer = firstChunk
-        if(len(rollingBuffer) == 0):
-            break
-        if(rollingBuffer.find(b'Content-Length') >= 0):
-            dataChunks.append(firstChunk)
-            posHeader = rollingBuffer.find(b'Content-Length') + 15
-            fileLen = int(rollingBuffer[posHeader : rollingBuffer.find(b'\r\n', posHeader)])
-            print('fileLen: ', fileLen)
-            offset = 0
-            if rollingBuffer.find(b'\r\n\r\n'):
-                BodyBegin = rollingBuffer.find(b'\r\n\r\n') + 4
-                offset = len(firstChunk) - BodyBegin + 1
-                if offset != fileLen:
-                    remain = (fileLen - offset)
-                    cntByteRecv = 0
-                    while cntByteRecv < remain:
-                        chunk = clientSocket.recv(min(2048, remain - cntByteRecv))
-                        cntByteRecv += len(chunk)
-                        dataChunks.append(chunk)
-                        # print('Downloading ', round(cntByteRecv / remain * 100, 2) , '%')
-                print("Finish receiving data...")
-                break
-        elif (rollingBuffer.find(b'Transfer-Encoding: chunked')) >= 0:
-            #append header
-            dataChunks.append(rollingBuffer[:rollingBuffer.find(b'\r\n\r\n')] + b'\r\n\r\n')
-            headerEndingPosition = rollingBuffer.find(b'\r\n\r\n') + len(b'\r\n\r\n')
-            rollingBuffer = rollingBuffer[headerEndingPosition:]
-            chunkSizePosition = rollingBuffer.find(b'\r\n')
-            chunkSize = int(rollingBuffer[:chunkSizePosition].decode(), 16)
-            rollingBuffer = rollingBuffer[(chunkSizePosition + len(b'\r\n')):]
-            while True:
-                if chunkSize == 0:
-                    break
-                chunk = clientSocket.recv(2048)
-                rollingBuffer += chunk
-                if len(rollingBuffer) >= chunkSize + 2:
-                    while len(rollingBuffer) >= chunkSize + 2:
-                        dataChunks.append(rollingBuffer[:chunkSize])
-                        rollingBuffer = rollingBuffer[(chunkSize + len(b'\r\n')):]
-                        while rollingBuffer.find(b'\r\n') == -1:
-                            chunk = clientSocket.recv(1)
-                            rollingBuffer += chunk
-                        chunkSizePosition = rollingBuffer.find(b'\r\n')
-                        chunkSize = int(rollingBuffer[:chunkSizePosition].decode(), 16)
-                        if chunkSize == 0:
-                            break
-                        rollingBuffer = rollingBuffer[(chunkSizePosition + len(b'\r\n')):]
-            break
-        else: break
-    result = b''.join(dataChunks)
-    return result
-
-def writeData(receiveMessage, fileName):
-    receiveMessage = receiveMessage.split(b'\r\n\r\n')
-    headers = receiveMessage[0]
-    data = b''
-    for i in range(1, len(receiveMessage)):
-        data += receiveMessage[i]
-    print(headers.decode())
-    # print(data.decode())
-    fileName = fileName.replace('%20', ' ')
-    f = open(fileName, "wb")
-    f.write(data)
-    f.close()
-
 def parseRequest(requestURL):
     if(requestURL[-1] != '/'):
         requestURL += '/'
@@ -98,27 +28,11 @@ def parseRequest(requestURL):
     isFolder = (fileName.find('.') == -1)
     requestURL = requestURL[:-1]
     return (serverName, fileName, isFolder)
-
-def readSubFolder(clientSocket, recvMessage):
-    queue = []
-    data = recvMessage.split(b'\r\n\r\n')[1]
-    for line in data.split(b'\n'):
-        if(line.find(b'href=') >= 0):
-            begPos = line.find(b'\"', line.find(b'href='))
-            endPos = line.find(b'\"', begPos + 1)
-            fileName = line[begPos + 1 : endPos]
-            if(fileName.find(b'.') >= 0):
-                queue.append(fileName)
-    return queue
  
 def clientProcess(clientSocket, requestURL, serverName, fileName, isFolder):
     time.sleep(0.2)
-    try:
-        #connect to socket
-        clientSocket.connect((serverName, serverPort))
-        print("Connects successfully")
 
-        #send request to server
+    def clientRequest(clientSocket, requestURL, serverName):
         requestMessage = "GET " + requestURL + " HTTP/1.1\r\n"
         connectionType = "Connection: keep-alive\r\n"
         hostName = "Host: " + serverName + "\r\n"
@@ -128,14 +42,15 @@ def clientProcess(clientSocket, requestURL, serverName, fileName, isFolder):
         
         iResult = clientSocket.send(requestMessage.encode())
         print("Request message: ", requestMessage)
-        # print("Byte send: ", iResult)
-
-        #receive first message
+        print("Byte send: ", iResult)
+    
+    def messageReceive(clientSocket):
         dataChunks = []
         while True:
             firstChunk = clientSocket.recv(MAXBUF)
             if(firstChunk.find(b'404 Not Found') >= 0):
                 raise Exception('File not found in server...')
+            # print(firstChunk)
             rollingBuffer = firstChunk
             if(len(rollingBuffer) == 0):
                 break
@@ -147,7 +62,7 @@ def clientProcess(clientSocket, requestURL, serverName, fileName, isFolder):
                 offset = 0
                 if rollingBuffer.find(b'\r\n\r\n'):
                     BodyBegin = rollingBuffer.find(b'\r\n\r\n') + 4
-                    offset = len(firstChunk) - BodyBegin + 1
+                    offset = len(firstChunk) - BodyBegin
                     if offset != fileLen:
                         remain = (fileLen - offset)
                         cntByteRecv = 0
@@ -159,6 +74,7 @@ def clientProcess(clientSocket, requestURL, serverName, fileName, isFolder):
                     print("Finish receiving data...")
                     break
             elif (rollingBuffer.find(b'Transfer-Encoding: chunked')) >= 0:
+                #append header
                 dataChunks.append(rollingBuffer[:rollingBuffer.find(b'\r\n\r\n')] + b'\r\n\r\n')
                 headerEndingPosition = rollingBuffer.find(b'\r\n\r\n') + len(b'\r\n\r\n')
                 rollingBuffer = rollingBuffer[headerEndingPosition:]
@@ -184,122 +100,68 @@ def clientProcess(clientSocket, requestURL, serverName, fileName, isFolder):
                             rollingBuffer = rollingBuffer[(chunkSizePosition + len(b'\r\n')):]
                 break
             else: break
-        receiveMessage = b''.join(dataChunks)
-        
+        result = b''.join(dataChunks)
+        return result
+    
+    def writeData(receiveMessage, fileName):
+        receiveMessage = receiveMessage.split(b'\r\n\r\n')
+        headers = receiveMessage[0]
+        data = b''
+        for i in range(1, len(receiveMessage)):
+            data += receiveMessage[i]
+        print(headers.decode())
+        # print(data.decode())
+        fileName = fileName.replace('%20', ' ')
+        f = open(fileName, "wb")
+        f.write(data)
+        f.close()
+    
+    def readSubFolder(clientSocket, recvMessage):
+        queue = []
+        data = recvMessage.split(b'\r\n\r\n')[1]
+        for line in data.split(b'\n'):
+            if(line.find(b'href=') >= 0):
+                begPos = line.find(b'\"', line.find(b'href='))
+                endPos = line.find(b'\"', begPos + 1)
+                fileName = line[begPos + 1 : endPos]
+                if(fileName.find(b'.') >= 0):
+                    queue.append(fileName)
+        return queue
+    
+    try:
+        #connect to socket
+        clientSocket.connect((serverName, serverPort))
+        print("Connects successfully")
+
+        #send request to server
+        clientRequest(clientSocket, requestURL, serverName)
+
+        #receive first message
+        receiveMessage = messageReceive(clientSocket)
+        # print(receiveMessage)
+        folderName = serverName + '_' + fileName
         if(isFolder == False):
             os.chdir(curDir)
             #write received data to file
-            temp = receiveMessage.split(b'\r\n\r\n')
-            headers = temp[0]
-            data = b''
-            for i in range(1, len(temp)):
-                data += temp[i]
-            print(headers.decode())
-            # print(data.decode())
-            fileName = fileName.replace('%20', ' ')
-            f = open(serverName + '_' + fileName, "wb")
-            f.write(data)
-            f.close()
+            writeData(receiveMessage, folderName)
         else:
-            folderName = serverName + '_' + fileName
             if(os.path.dirname(curDir) != curDir):
                 os.chdir(curDir)
             if(os.path.isdir(folderName) == False):
-                os.mkdir(serverName + '_' + fileName)
+                os.mkdir(folderName)
             os.chdir(folderName)
-            # writeData(receiveMessage, 'index.html')
+
+            writeData(receiveMessage, 'index.html')
 
             #push all items in subfolder to a queue
-            URLqueue = []
-            data = receiveMessage.split(b'\r\n\r\n')[1]
-            for line in data.split(b'\n'):
-                if(line.find(b'href=') >= 0):
-                    begPos = line.find(b'\"', line.find(b'href='))
-                    endPos = line.find(b'\"', begPos + 1)
-                    itemName = line[begPos + 1 : endPos]
-                    if(itemName.find(b'.') >= 0):
-                        URLqueue.append(itemName)
+            URLqueue = readSubFolder(clientSocket, receiveMessage)
 
             for request in URLqueue:
-                #receive \n
-                t = clientSocket.recv(1)
-                #request
-                requestMessage = "GET " + requestURL + request.decode() + " HTTP/1.1\r\n"
-                connectionType = "Connection: keep-alive\r\n"
-                hostName = "Host: " + serverName + "\r\n"
-                requestMessage += hostName
-                requestMessage += connectionType
-                requestMessage += "\r\n"
-                clientSocket.send(requestMessage.encode())
-                print("Request message: ", requestMessage)
-
-                dataChunks = []
-                while True:
-                    firstChunk = clientSocket.recv(MAXBUF)
-                    if(firstChunk.find(b'404 Not Found') >= 0):
-                        raise Exception('File not found in server...')
-                    rollingBuffer = firstChunk
-                    if(len(rollingBuffer) == 0):
-                        break
-                    if(rollingBuffer.find(b'Content-Length') >= 0):
-                        dataChunks.append(firstChunk)
-                        posHeader = rollingBuffer.find(b'Content-Length') + 15
-                        fileLen = int(rollingBuffer[posHeader : rollingBuffer.find(b'\r\n', posHeader)])
-                        print('fileLen: ', fileLen)
-                        offset = 0
-                        if rollingBuffer.find(b'\r\n\r\n'):
-                            BodyBegin = rollingBuffer.find(b'\r\n\r\n') + 4
-                            offset = len(firstChunk) - BodyBegin + 1
-                            if offset != fileLen:
-                                remain = (fileLen - offset)
-                                cntByteRecv = 0
-                                while cntByteRecv < remain:
-                                    chunk = clientSocket.recv(min(2048, remain - cntByteRecv))
-                                    cntByteRecv += len(chunk)
-                                    dataChunks.append(chunk)
-                                    # print('Downloading ', round(cntByteRecv / remain * 100, 2) , '%')
-                            print("Finish receiving data...")
-                            break
-                    elif (rollingBuffer.find(b'Transfer-Encoding: chunked')) >= 0:
-                        dataChunks.append(rollingBuffer[:rollingBuffer.find(b'\r\n\r\n')] + b'\r\n\r\n')
-                        headerEndingPosition = rollingBuffer.find(b'\r\n\r\n') + len(b'\r\n\r\n')
-                        rollingBuffer = rollingBuffer[headerEndingPosition:]
-                        chunkSizePosition = rollingBuffer.find(b'\r\n')
-                        chunkSize = int(rollingBuffer[:chunkSizePosition].decode(), 16)
-                        rollingBuffer = rollingBuffer[(chunkSizePosition + len(b'\r\n')):]
-                        while True:
-                            if chunkSize == 0:
-                                break
-                            chunk = clientSocket.recv(2048)
-                            rollingBuffer += chunk
-                            if len(rollingBuffer) >= chunkSize + 2:
-                                while len(rollingBuffer) >= chunkSize + 2:
-                                    dataChunks.append(rollingBuffer[:chunkSize])
-                                    rollingBuffer = rollingBuffer[(chunkSize + len(b'\r\n')):]
-                                    while rollingBuffer.find(b'\r\n') == -1:
-                                        chunk = clientSocket.recv(1)
-                                        rollingBuffer += chunk
-                                    chunkSizePosition = rollingBuffer.find(b'\r\n')
-                                    chunkSize = int(rollingBuffer[:chunkSizePosition].decode(), 16)
-                                    if chunkSize == 0:
-                                        break
-                                    rollingBuffer = rollingBuffer[(chunkSizePosition + len(b'\r\n')):]
-                        break
-                    else: break
-                receiveMessage = b''.join(dataChunks)
-                os.chdir(curDir + '/' + folderName)
-                temp = receiveMessage.split(b'\r\n\r\n')
-                headers = temp[0]
-                data = b''
-                for i in range(1, len(temp)):
-                    data += temp[i]
-                print(headers.decode())
-                # print(data.decode())
-                # print(fileName)
                 fileName = request.decode().replace('%20', ' ')
-                f = open(fileName, "wb")
-                f.write(data)
-                f.close()
+                clientRequest(clientSocket, requestURL + request.decode(), serverName)
+                receiveMessage = messageReceive(clientSocket)
+                os.chdir(curDir + '\\' + folderName)
+                writeData(receiveMessage, fileName)
             
     except Exception as e:
         print(e)
